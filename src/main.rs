d641fba5943736e9442c694dcf03e1850306eb23
Alejandro Miranda <alejandro.miranda@hey.com>
2022-01-02 14:29:09 -0500

    builds at this point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> !{
+    // println!("Hello, world!");
+    defmt::info!("Hello World");
+    exit();
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    defmt::error!("panicked");
+    exit()
+}
+
+pub fn exit() -> ! {
+    loop {
+        cortex_m::asm::bkpt(); 
+    }
 }
