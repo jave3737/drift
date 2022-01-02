98831c6c22dc2dbdcafc87e85342598f48eddaa7
Alejandro Miranda <alejandro.miranda@hey.com>
2022-01-02 18:56:39 -0500

    Adding core peripherals and boot settings

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,32 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use hal::{clocks::Clock as _, pac, watchdog::Watchdog};
+use rp2040_hal as hal;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
 
 #[entry]
-fn main() -> !{
+fn main() -> ! {
     // println!("Hello, world!");
     defmt::info!("Hello World");
+
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut pac = pac::Peripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut &mut watchdog,
+    ).ok().unwrap();
     exit();
 }
 
@@ -18,6 +39,6 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 pub fn exit() -> ! {
     loop {
-        cortex_m::asm::bkpt(); 
+        cortex_m::asm::bkpt();
     }
 }
